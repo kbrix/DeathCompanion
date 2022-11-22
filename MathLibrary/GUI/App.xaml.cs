0822dbed8c1902e2df12b92c8dd141b2bc2fd850
kbrix <kbrix@protonmail.com>
2022-11-22 21:01:42 +0100

    Add MAUI skeleton app

---
+++ MathLibrary/GUI/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace GUI;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ MathLibrary/GUI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace GUI;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
